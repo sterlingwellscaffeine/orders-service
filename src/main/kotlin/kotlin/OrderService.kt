027787983386a3b06ace935462144a7e6f918314
Sterling Wells <shoshonte@gmail.com>
2020-10-17 17:38:41 -0400

    Step 3: Build a new customer notification system

--- src/main/kotlin/kotlin/OrderService.kt
+++ src/main/kotlin/kotlin/OrderService.kt
@@ -1,13 +1,16 @@
 import OfferService.FruitOffer
 import lib.InputReader
 import lib.OutputPrinter
+import kotlin.reflect.KClass
 
 fun main(vararg args: String) {
 
         val inputReader = InputReader()
         val outputPrinter = OutputPrinter()
         val fruitOrder = FruitOffer()
-
+        MailServiceChannel.subscribe{
+                message: Message -> println("${message.status}: ${message.deliveryTimeMessage}")
+        }
         do {
                 // read in all of the fruits and add them to the fruit array
                 val line = inputReader.readLn().replace("\\s+".toRegex(), "").replace("[", "")
@@ -22,7 +25,33 @@ fun main(vararg args: String) {
                 // add up all of the fruits based on their mapping
                 val sum = fruitOrder.addFruits(fruits)
                 outputPrinter.printline("\$$sum")
+                MailServiceChannel.publish(Message("fruit order processed", "delivery time = 3-5 business days"))
         }
         while (true)
+
+
 }
+
+object MailServiceChannel {
+
+        val subscribers: MutableMap<KClass<*>, (Any) -> Unit> = mutableMapOf()
+
+        inline fun <reified T : Any> publish(event: T) {
+                subscribers[T::class]?.invoke(event)
+        }
+
+        inline fun <reified T> subscribe(noinline subscriber: (T) -> Unit) {
+                subscribers[T::class] = subscriber as (Any) -> Unit
+        }
+
+        // expects 2 args: <topic> and <count>
+        private fun publishSuccessMessage(vararg args: String) {
+
+
+
+        }
+
+}
+
+data class Message(val status: String, val deliveryTimeMessage: String)
 
